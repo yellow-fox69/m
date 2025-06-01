de1dbe723654a1458fe10d256c3e94050a0d7d4e
yellow-fox69 <smmcollab221@gmail.com>
2025-06-01 21:04:38 +0530

    Update Krushkals.c

--- Krushkals.c
+++ Krushkals.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define INF 999
-
 int parent[100], cost[100][100];
-
 int findParent(int i) {
     while (parent[i] != 0) {
         i = parent[i];
     }
     return i;
 }
-
 int  unionVertices(int  i, int  j) {
     if (i != j) {
         parent[j] = i;
@@ -19,14 +15,11 @@ int  unionVertices(int  i, int  j) {
     }
     return 0;
 }
-
 int main() {
     int i, j, n, min, ne = 1;
     int u = 0, v = 0, a = 0, b = 0, mincost = 0;
-
     printf("Enter the number of vertices/nodes in the graph\n");
     scanf("%d", &n);
-
     printf("Enter the Cost/Weight matrix\n");
     for (i = 1; i <= n; i++) {
         parent[i] = 0;
@@ -37,7 +30,6 @@ int main() {
             }
         }
     }
-
     printf("The edges of Minimum spanning tree are:\n");
     while (ne < n) {
         min = INF;
@@ -50,7 +42,6 @@ int main() {
                 } } }
         u = findParent(u);
         v = findParent(v);
-
         if (unionVertices(u, v)) {
             printf("%d Edge Selected (%d --- %d) Cost = %d\n", ne++, a, b, min);
             mincost += min;
@@ -58,6 +49,5 @@ int main() {
         cost[a][b] = cost[b][a] = INF;
     }
     printf("Minimum cost = %d\n", mincost);
-
     return 0;
 }
